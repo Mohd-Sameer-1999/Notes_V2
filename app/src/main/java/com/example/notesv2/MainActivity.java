85bea53bf6b6591e2e3ebcd619d2a4c284870586
Mohd-Sameer <mohdsameer2006@gmail.com>
2020-10-13 19:03:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notesv2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.notesv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        
+    }
+
+
+}
